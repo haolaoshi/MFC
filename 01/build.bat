7a09ad0070021ebfac4f00972cefed6d7bbeaf1b
haolaoshi <liujiahao.oak@gmail.com>
2021-04-05 00:40:40 +0800

    mfc02

--- 01/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem  invoke from "Development command prompt for vs"
-del *.obj  
-del *.exe  
-del *.res  
-nmake generic.mak 
-del *.obj  
-del *.res 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+git add
+
 01	Generic.mak
 	makefile:
